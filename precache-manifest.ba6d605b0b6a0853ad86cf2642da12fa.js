cf04bc1e3f096ec85dd39cd29142143fb9600aae
tangweikun <819573105@qq.com>
2019-11-15 13:57:01 +0800

    Updates

---
+++ precache-manifest.ba6d605b0b6a0853ad86cf2642da12fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33aff11d5d17663c3587fb99a137d094",
+    "url": "/art-gallery/index.html"
+  },
+  {
+    "revision": "b0fb68b232eb42122ead",
+    "url": "/art-gallery/static/css/main.de53e1fd.chunk.css"
+  },
+  {
+    "revision": "d02af0417fd1b59d4b4a",
+    "url": "/art-gallery/static/js/2.a5a6297e.chunk.js"
+  },
+  {
+    "revision": "b0fb68b232eb42122ead",
+    "url": "/art-gallery/static/js/main.a8d9910d.chunk.js"
+  },
+  {
+    "revision": "b49895dba0cee94900f6",
+    "url": "/art-gallery/static/js/runtime-main.01129a7d.js"
+  },
+  {
+    "revision": "04f7f48684694ac159c019f312bbfba8",
+    "url": "/art-gallery/static/media/github.04f7f486.svg"
+  },
+  {
+    "revision": "1102dd99a54631cb0aa5b09228e5f604",
+    "url": "/art-gallery/static/media/website.1102dd99.svg"
+  }
+]);
