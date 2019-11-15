3684bef7b6e73b49eadc590448ced2471c90d3cd
tangweikun <819573105@qq.com>
2019-11-15 13:52:48 +0800

    :construction: work on LinkCard

--- src/components/LinkCard.js
+++ src/components/LinkCard.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import styles from './LinkCard.module.scss';
+import React from "react";
+import styles from "./LinkCard.module.scss";
+import GITHUB_ICON from "../assets/github.svg";
+import WEBSITE_ICON from "../assets/website.svg";
 
 export function LinkCard(props) {
-  const { style = {} } = props;
-
   return (
     <div
-      style={style}
-      className={styles.wrapper}
-      onClick={() => window.open(props.url)}
+      className={styles.card}
+      // onClick={() => window.open(props.url)}
     >
-      {props.title}
+      <div style={{ background: props.bgColor }} className={styles.header}>
+        {props.title}
+      </div>
+      <div className={styles.desc}>{props.desc}</div>
+      <div className={styles.links}>
+        <img
+          src={WEBSITE_ICON}
+          alt="website"
+          style={{ marginRight: "20px" }}
+          onClick={() => window.open(props.url)}
+        />
+        <img
+          src={GITHUB_ICON}
+          alt="github"
+          onClick={() => window.open(props.repo)}
+        />
+      </div>
     </div>
   );
 }
--- src/mock/siteList.js
+++ src/mock/siteList.js
@@ -1,16 +1,42 @@
 export const SITE_LIST = [
   {
-    url: 'https://dolphin.surge.sh/',
-    logo: '',
-    name: 'dolphin',
-    description: '',
-    repo: '',
+    url: "https://dolphin.surge.sh/",
+    logo: "",
+    name: "Dolphin",
+    description: "JavaScript原生API的polyfill",
+    repo: "https://github.com/tangweikun/dolphin",
+    bgColor: "rgb(255, 114, 129)"
   },
   {
-    url: 'http://building-block.surge.sh/',
-    logo: '',
-    name: 'building-block',
-    description: '',
-    repo: '',
+    url: "http://building-block.surge.sh/",
+    logo: "",
+    name: "Building Block",
+    description: "一个简单的组件库",
+    repo: "https://github.com/tangweikun/building-block",
+    bgColor: "rgb(64, 196, 255)"
   },
+  {
+    url: "https://tangweikun.github.io/happy-beauty-stars/",
+    logo: "",
+    name: "Beauty Stars",
+    description: "一个简单的评分组件",
+    repo: "https://github.com/tangweikun/happy-beauty-stars",
+    bgColor: "rgb(255, 157, 68)"
+  },
+  {
+    url: "https://tangweikun.github.io/happy-clicker/",
+    logo: "",
+    name: "Clicker",
+    description: "一个简单的计数器，可以'加一'，'减一'和'重置'",
+    repo: "https://github.com/tangweikun/happy-clicker",
+    bgColor: "rgb(140, 216, 66)"
+  }
+  // {
+  //   url: "https://tangweikun.github.io/happy-clicker/",
+  //   logo: "",
+  //   name: "Clicker",
+  //   description: "",
+  //   repo: "",
+  //   bgColor: "rgb(140, 216, 66)"
+  // }
 ];
